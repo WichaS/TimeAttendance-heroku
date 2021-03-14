c7b36398ffcc4e5a25f8ff474f81181278f8d66d
Wicha_Home <pha23773@gmail.com>
2021-03-14 20:17:29 +0700

    cleancode

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 
-var i = "iconext"; // Issuer (Software organization who issues the token)
-var s = "sippavit"; // Subject (intended user of the token)
-var a = "http://iconext.co.th"; // Audience (Domain within which this token will live and function)
+var i = "iconext";
+var s = "wichayut";
+var a = "http://iconext.co.th";
 
 var publicKEY = fs.readFileSync(path.join(__dirname + "/public.key"), "utf8");
 var privateKEY = fs.readFileSync(path.join(__dirname + "/private.key"), "utf8");
